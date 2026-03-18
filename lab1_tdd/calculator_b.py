459530de1804dd63f23539f68fe67c65985f680d
MaksyKost <maksym03112005@gmail.com>
2026-03-18 20:31:55 +0100

    Add new function to calculator

--- lab1_tdd/calculator_b.py
+++ lab1_tdd/calculator_b.py
@@ -2,10 +2,10 @@ def Add(number):
     if number == "":
         return 0
     result = 0
+    number = number.replace("\n", ",")
     parts = number.split(",")
     for x in parts:
         if x == "":
             raise ValueError("Bad string!")
         result += int(x)
     return result
-
