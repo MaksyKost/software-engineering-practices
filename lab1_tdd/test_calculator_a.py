169ab5ee9113dd0a9432f1ab821a1eceecf4e40b
Ol3hhh <olehmaru@gmail.com>
2026-03-18 19:38:58 +0100

    add more tests in UT

--- lab1_tdd/test_calculator_a.py
+++ lab1_tdd/test_calculator_a.py
@@ -12,7 +12,13 @@ class TestAdd(unittest.TestCase):
 
     def test_dual_number(self):
         self.assertEqual(Add("5,3"), 8)
-
+    
+    def test_multi_number(self):
+        self.assertEqual(Add("1,2,3,4,5,6"), 21)
+    
+    def test_value(self):
+        with self.assertRaises(ValueError):
+            Add("I use Arch btw!")
 
 if __name__ == "__main__":
     unittest.main()
