5f3da9a159322bdbaad80075b740e1b3df4ddcab
MaksyKost <maksym03112005@gmail.com>
2026-03-18 20:15:36 +0100

    Update calculator

--- lab1_tdd/calculator_b.py
+++ lab1_tdd/calculator_b.py
@@ -1,6 +1,11 @@
 def Add(number):
     if number == "":
         return 0
+    result = 0
+    parts = number.split(",")
+    for x in parts:
+        if x == "":
+            raise ValueError("Bad string!")
+        result += int(x)
+    return result
 
-    return sum(int(x) for x in number.split(","))
-        
